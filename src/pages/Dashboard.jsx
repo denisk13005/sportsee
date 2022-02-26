348261474ffa8830f10bccb422bafa78d33f2d9f
kirmann <k.dkle@hotmail.fr>
2022-02-26 15:04:13 +0100

    structure du dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,9 +8,21 @@ const Dashboard = () => {
   return (
     <div>
       <TopNav />
+      <LeftNavBar />
       <div className="mainContent">
-        <LeftNavBar />
         <Header userName={'Thomas'} />
+        <div className="graphsAndDailyContainer">
+          <section className="graphsContainer">
+            <div className="dailyActivity"></div>
+            <div className="otherGraph"></div>
+          </section>
+          <aside className="dailyIntake">
+            <div className="cal"></div>
+            <div className="prot"></div>
+            <div className="gluc"></div>
+            <div className="lip"></div>
+          </aside>
+        </div>
       </div>
     </div>
   )
