088eae4eb93c7e170481d68bb1ebb538848cd5e7
denisk13005 <kirmanndenis@gmail.com>
2022-06-15 13:23:35 +0200

    mobile graph ok

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -11,7 +11,7 @@ import PropTypes from 'prop-types'
  */
 const CardInfo = ({ icone, number, unit, type }) => {
   return (
-    <div className="cardInfoContainer" style={{ backgroundColor: 'lightBlue' }}>
+    <div className="cardInfoContainer">
       <div className="imgContainer" id={type}>
         <img src={icone} alt="icone" />
       </div>
